d1e31788e7694c7106aff914ca56b42c54e50d83
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2025-12-29 22:13:17 +0530

    add task in the UI on button click,rectified the wrong id in js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const taskInput = document.getElementById('taskInput');
-const addBtn = document.getElementById('addBtn');
+const addBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
+
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if ( taskText === "" ) return;
+
+    const listItem = document.createElement('li');
+    listItem.innerText = taskText;
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+});
